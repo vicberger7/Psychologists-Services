e13739e53a0b77e3ce950fac199a2db5f6c7c44b
vicberger7 <vicberger7@gmail.com>
2024-11-02 08:36:14 +0100

    html update

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-
+import 'modern-normalize/modern-normalize.css';
 import { BrowserRouter } from 'react-router-dom';
 import { HelmetProvider } from 'react-helmet-async';
 import { Provider } from 'react-redux';
